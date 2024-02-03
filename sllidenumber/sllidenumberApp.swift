3be495a5f42c985050630304a4747dd5b5030152
fusethnd <fusethnd@gmail.com>
2024-02-04 01:09:15 +0700

    Initial Commit

---
+++ sllidenumber/sllidenumberApp.swift
@@ -0,0 +1,17 @@
+//
+//  sllidenumberApp.swift
+//  sllidenumber
+//
+//  Created by Thanadon Boontawee on 4/2/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct sllidenumberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ sllidenumberUITests/sllidenumberUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  sllidenumberUITestsLaunchTests.swift
+//  sllidenumberUITests
+//
+//  Created by Thanadon Boontawee on 4/2/2567 BE.
+//
+
+import XCTest
+
+final class sllidenumberUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
